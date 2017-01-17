89e1f9c38b430f549c0342da495a3cd3e75a84ac
Jerry Liu <jerry.liu@shopify.com>
2017-01-17 15:37:16 -0500

    benchmark render by itself

--- performance/benchmark.rb
+++ performance/benchmark.rb
@@ -4,8 +4,12 @@ require_relative 'theme_runner'
 Liquid::Template.error_mode = ARGV.first.to_sym if ARGV.first
 profiler = ThemeRunner.new
 
+# this profiler is for benchmarking render only
+profiler_render = ThemeRunner.new
+profiler_render.compile_tests
+
 Benchmark.ips do |x|
-  x.time = 60
+  x.time = 10
   x.warmup = 5
 
   puts
@@ -13,5 +17,6 @@ Benchmark.ips do |x|
   puts
 
   x.report("parse:") { profiler.compile }
-  x.report("parse & run:") { profiler.run }
+  x.report("render:") { profiler_render.benchmark_render }
+  x.report("parse & render:") { profiler.run }
 end
